314040cbb1a1458667eaa88605fb5c38b03864fb
Rishav-Chanda <your_email_address@example.com>
2021-08-09 13:12:07 +0530

    added the basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react'
 import './App.css';
+import axios from 'axios';
+import SearchIcon from '@material-ui/icons/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
+
+  useEffect(() => {
+      fetchItems();
+  }, []);
+
+  const fetchItems = async () =>{
+    const result = await axios(
+      `https://www.breakingbadapi.com/api/characters`
+    )
+    console.log(result.data)
+    setItems(result.data)
+    setIsLoading(false)
+  } 
+
+ return (
+    <div className="container">
+      <div className="header">
+        <div>Breaking Bad</div>
+      </div>
+      <div className="search-bar">
+        <input type="text"/>
+        <SearchIcon className="search-bar-icon"/>
+      </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
